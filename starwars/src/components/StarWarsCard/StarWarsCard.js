8795cd5798caf00b4011b3c4e0c5ca91bc7e6cc8
Priya Vaidya <pvaidya56@gmail.com>
2019-09-30 16:09:07 -0700

    Used planet api

--- starwars/src/components/StarWarsCard/StarWarsCard.js
+++ starwars/src/components/StarWarsCard/StarWarsCard.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
-
+import Planet from '../Planet'
 function StarWarsCard(props) {
   console.log('props', props)
   return (
     <div>
       <h2>{props.person.name}</h2>
-      <p>{props.person.homeworld}</p>
+      <Planet planet={props.person.homeworld} />
     </div>
   )
 }
--- starwars/src/components/StarwarsCharacters.js
+++ starwars/src/components/StarwarsCharacters.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import StarWarsCard from './StarWarsCard';
+import StarWarsCard from './StarWarsCard/StarWarsCard';
 
 function StarWarsCharacters() {
   const [character, setCharacter] = useState([]);
   const [homeworld, setHomeworld] = useState([]);
 
   useEffect(() => {
-
+    // setPeople([]);
     axios
-      .get('https://swapi.co/api/people')
+      .get('https://swapi.co/api/people/')
       .then(data => {
         console.log('API callresult', data.data.people)
         setCharacter(data.data.results)
